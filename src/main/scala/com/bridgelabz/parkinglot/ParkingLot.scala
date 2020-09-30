5d04990cc220ffe46582d3fecc72882de9c0d97d
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-30 19:26:13 +0530

    [Ranbir]REFACTORED:Code to handle exceptions

--- src/main/scala/com/bridgelabz/parkinglot/ParkingLot.scala
+++ src/main/scala/com/bridgelabz/parkinglot/ParkingLot.scala
@@ -1,6 +1,7 @@
 package com.bridgelabz.parkinglot
 
 import java.util
+
 class ParkingLot {
   val parkingList = new util.ArrayList[String]
 
@@ -8,4 +9,12 @@ class ParkingLot {
     parkingList.add(carNumber)
     true
   }
+
+  def unPark(carNumber: String): Boolean = {
+    if (!parkingList.contains(carNumber)){
+      throw new ParkingLotException(ParkingLotExceptionEnums.CAR_NUMBER_MISMATCH)
+    }
+    parkingList.remove(carNumber)
+  }
+
 }
