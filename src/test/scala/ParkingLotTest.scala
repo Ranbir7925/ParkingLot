4f791a17982ce4f38986e4c7c5a7b20aab815bec
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-30 19:43:49 +0530

    [Ranbir]ADDED:Test case to handlecustomexception if the parking is full

--- src/test/scala/ParkingLotTest.scala
+++ src/test/scala/ParkingLotTest.scala
@@ -6,20 +6,32 @@ class ParkingLotTest extends FunSuite{
   test("givenCarDetails_WhenAddedToParkingLot_ShouldReturnTrue"){
     val parkingLot = new ParkingLot()
     val parked = parkingLot.park("MH01-3241")
-    assert(parked == true)
+    assert(parked)
   }
 
   test("givenCarDetails_WhenProvidedToUnParkTheCar_ShouldReturnTrue"){
     val parkingLot = new ParkingLot()
     val unParked = parkingLot.park("MH01-3241")
-    assert(unParked == true)
+    assert(unParked)
   }
 
   test("givenIncorrectCarDetails_WhenProvidedToUnParkTheCar_ShouldGiveException"){
     val throws = intercept[ParkingLotException]{
       val parkingLot = new ParkingLot()
+      parkingLot.park("MH01-2255")
       parkingLot.unPark("MH01-5475")
     }
-    assert(throws.getMessage == ParkingLotExceptionEnums.CAR_NUMBER_MISMATCH.toString)
+    assert(throws.getMessage == ParkingLotExceptionEnums.carNumberMissMatch.toString)
+  }
+
+  test("givenCarDetails_WhenTheParkingIsFull_ShouldGiveException"){
+    val throws = intercept[ParkingLotException]{
+      val parkingLot = new ParkingLot()
+      parkingLot.park("MH01-2348")
+      parkingLot.park("MH01-5348")
+      parkingLot.park("MH01-4348")
+      parkingLot.park("MH01-9348")
+    }
+    assert(throws.getMessage == ParkingLotExceptionEnums.parkingFull.toString)
   }
 }
