9eee5c2ef6915085ac35c81d686d73d42f370e61
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-30 19:27:09 +0530

    [Ranbir]ADDED:Test case to handle custom exception

--- src/test/scala/ParkingLotTest.scala
+++ src/test/scala/ParkingLotTest.scala
@@ -1,11 +1,25 @@
-import com.bridgelabz.parkinglot.ParkingLot
+import com.bridgelabz.parkinglot.{ParkingLot, ParkingLotException, ParkingLotExceptionEnums}
 import org.scalatest.FunSuite
 
 class ParkingLotTest extends FunSuite{
 
   test("givenCarDetails_WhenAddedToParkingLot_ShouldReturnTrue"){
-    val parkingLot = new ParkingLot
+    val parkingLot = new ParkingLot()
     val parked = parkingLot.park("MH01-3241")
     assert(parked == true)
+  }
+
+  test("givenCarDetails_WhenProvidedToUnParkTheCar_ShouldReturnTrue"){
+    val parkingLot = new ParkingLot()
+    val unParked = parkingLot.park("MH01-3241")
+    assert(unParked == true)
+  }
+
+  test("givenIncorrectCarDetails_WhenProvidedToUnParkTheCar_ShouldGiveException"){
+    val throws = intercept[ParkingLotException]{
+      val parkingLot = new ParkingLot()
+      parkingLot.unPark("MH01-5475")
+    }
+    assert(throws.getMessage == ParkingLotExceptionEnums.CAR_NUMBER_MISMATCH.toString)
   }
 }
