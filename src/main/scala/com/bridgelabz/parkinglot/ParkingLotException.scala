8631dcd2f51535a71d26324cd8e8ba3f19e2fbcd
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-30 22:01:10 +0530

    [Ranbir]REFACTORED:Code to give exception message if same car number added

--- src/main/scala/com/bridgelabz/parkinglot/ParkingLotException.scala
+++ src/main/scala/com/bridgelabz/parkinglot/ParkingLotException.scala
@@ -5,5 +5,5 @@ class ParkingLotException(message: ParkingLotExceptionEnums.Value) extends Excep
     type ParkingLotException = Value
 
     val carNumberMissMatch = Value("Car for a given car number is not found")
-    val parkingFull = Value("Parking is full.")
+    val sameCarNumber = Value("Car cannot be parked as car number is same")
   }
